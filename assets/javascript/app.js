ab7ddbcdccf85e0b640a42eb1ce54cb078d35b4c
BlaiseAK <blaisekress@yahoo.com>
2018-06-16 11:40:37 -0400

    put together most of database and added all data to upload. What is still needed is to do the math in the upload click button so the information only needs to be pulled on loading of the page

---
+++ assets/javascript/app.js
@@ -0,0 +1,73 @@
+var config = {
+    apiKey: "AIzaSyCnB9sutksxZSmAimUSxh_8zReI-oFdshY",
+    authDomain: "traintime-app.firebaseapp.com",
+    databaseURL: "https://traintime-app.firebaseio.com",
+    projectId: "traintime-app",
+    storageBucket: "traintime-app.appspot.com",
+    messagingSenderId: "46561242414"
+  };
+  firebase.initializeApp(config);
+
+//   Storing direction to firebase for later
+  var database = firebase.database();
+
+//  Establish click function to add Train information
+$(document).on("click", ".btn-primary", function() {
+    // Allows for both button click and 'enter' key to pre pressed to submit information
+    event.preventDefault();
+
+    // Create variables to store user inputs
+    var trainName = $("#nameInput").val().trim();
+    var trainDestination = $("#destinationInput").val().trim();
+    var trainStart = $("#timeInput").val().trim();
+    var trainFrequency = $("#frequencyInput").val().trim();
+
+    // Create an object to store our new variables inside of.
+    var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        start: trainStart,
+        frequency: trainFrequency,
+    };
+
+    // Push newTrain information up to the database
+    database.ref().push(newTrain);
+
+    // Console logging this to see if they actually take.
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.start);
+    console.log(newTrain.frequency);
+
+    // Alert to let user know we have stored the info.
+    alert(newTrain.name + "  has sucessfuly been added to the table");
+
+    // Clears the inputs
+    $("#nameInput").val("");
+    $("#destinationInput").val("");
+    $("#timeInput").val("");
+    $("#frequencyInput").val("");
+
+})
+
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+    console.log(childSnapshot.val());
+
+    // Store new train info into variables.
+    var dataName = childSnapshot.val().name;
+    var dataDestination = childSnapshot.val().destination;
+    var dataTime = childSnapshot.val().start;
+    var dataFrequency = childSnapshot.val().frequency;
+
+    // See if the information is calling back
+    console.log(dataName);
+    console.log(dataDestination);
+    console.log(dataTime);
+    console.log(dataFrequency);
+
+    // Add each train's data into the table
+    // empMonths needs to be changed to next arrival time and empBilled needs to be Minutes away
+  $("#trainTable > tbody").append("<tr><td>" + dataName + "</td><td>" + dataDestination + "</td><td>" + dataFrequency + "</td><td>" + empMonths + "</td><td>" + empBilled + "</td></tr>");
+
+})
